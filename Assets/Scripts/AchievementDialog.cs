6ec896c6ed57b0d580b9bccb24e59cf3f454aa0b
Brad-F <bradleyf@bitheads.com>
2022-09-02 12:13:01 -0400

    Initial commit for Lesson-4

---
+++ Assets/Scripts/AchievementDialog.cs
@@ -0,0 +1,15 @@
+// Copyright 2022 bitHeads, Inc. All Rights Reserved.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementDialog : Dialog
+{
+    [SerializeField] private AchievementInfo[] achievementInfos;
+
+    protected override void OnShow()
+    {
+        //TODO: Implement Achievements
+    }
+}
---
+++ Assets/Scripts/Network.cs
@@ -0,0 +1,41 @@
+// Copyright 2022 bitHeads, Inc. All Rights Reserved.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class Network : MonoBehaviour
+{
+    public static Network sharedInstance;
+
+    private BrainCloudWrapper m_BrainCloud;
+
+
+    private void Awake()
+    {
+        sharedInstance = this;
+
+        DontDestroyOnLoad(gameObject);
+
+        // Create and initialize the BrainCloud wrapper
+        m_BrainCloud = gameObject.AddComponent<BrainCloudWrapper>();
+        m_BrainCloud.Init();
+
+        // Log the BrainCloud client version
+        Debug.Log("BrainCloud client version: " + m_BrainCloud.Client.BrainCloudClientVersion);
+    }
+
+    void Update()
+    {
+        // Make sure you invoke this method in Update, or else you won't get any callbacks
+        m_BrainCloud.RunCallbacks();
+    }
+
+    public bool IsAuthenticated()
+    {
+        return false;
+    }
+}
